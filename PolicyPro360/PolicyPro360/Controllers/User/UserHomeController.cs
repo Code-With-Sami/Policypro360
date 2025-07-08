c1f8ea34a10ae814da4e081b3538b9c55e4e021b
Kashaf Jamil <kashafcoder15@gmail.com>
2025-07-08 18:57:56 +0500

    user template integrate

--- PolicyPro360/PolicyPro360/Controllers/User/UserHomeController.cs
+++ PolicyPro360/PolicyPro360/Controllers/User/UserHomeController.cs
@@ -8,5 +8,90 @@ namespace PolicyPro360.Controllers.User
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult Faq()
+        {
+            return View();
+        }
+        public IActionResult Life()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Motor()
+        {
+            return View();
+        }
+        public IActionResult Medical()
+        {
+            return View();
+        }
+        public IActionResult Sign()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        public IActionResult MakePayment()
+        {
+            return View();
+        }
+        public IActionResult MyPolicies()
+        {
+            return View();
+        }
+        public IActionResult MyPolicyDetail()
+        {
+            return View();
+        }
+        public IActionResult ApplyLoan()
+        {
+            return View();
+        }
+        public IActionResult FileClaim()
+        {
+            return View();
+        }
+        public IActionResult MyApplication()
+        {
+            return View();
+        }
+        public IActionResult Support()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
     }
 }
